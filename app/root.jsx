62225f0ade9257a67087b005401f26b3c2d20572
Tajveez <Tajveez786@gmail.com>
2021-12-29 00:27:18 +0500

    running project

--- app/root.jsx
+++ app/root.jsx
@@ -1,31 +1,50 @@
 import {
-  Links,
+  Link,
   LiveReload,
-  Meta,
   Outlet,
-  Scripts,
-  ScrollRestoration
 } from "remix";
 
-export function meta() {
-  return { title: "New Remix App" };
-}
 
 export default function App() {
   return (
+    <Document title="Tajlogs">
+
+      <Layout>
+        <Outlet />
+      </Layout>
+    </Document>
+  );
+}
+
+function Document({ title, children }) {
+  return (
     <html lang="en">
       <head>
+        <title>{title ? title : 'Remix blog'}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
+        {children}
         {process.env.NODE_ENV === "development" && <LiveReload />}
       </body>
     </html>
-  );
+  )
+}
+
+function Layout({ children }) {
+  return (
+    <>
+      <nav className="navbar">
+        <Link to='/' className='logo'>Home</Link>
+        <ul className="nav">
+          <li>
+            <Link to='/posts'>Posts</Link>
+          </li>
+        </ul>
+      </nav>
+      {children}
+    </>
+
+  )
 }
